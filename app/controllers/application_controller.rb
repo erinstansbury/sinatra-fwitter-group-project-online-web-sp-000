81d39b5a03fb98d2b8e41ba3fcfbc54a7e5b1e42
Erin Stansbury <ekstansbury@gmail.com>
2021-03-05 05:22:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,29 @@ require './config/environment'
 require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    use Rack::Flash
+    set :session_secret, "fwitter_secret"
+  end
+
+  get '/' do
+  	erb :index
+  end
+
+  helpers do
+
+  	def is_logged_in?
+  		!!session[:user_id]
+  	end
+
+  	def current_user
+  		User.find(session[:user_id])
+  	end
+
+  end
+
+end
