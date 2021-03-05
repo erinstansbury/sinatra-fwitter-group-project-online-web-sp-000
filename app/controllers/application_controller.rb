ae66d2935683c3581c14899358be120c0be34ab2
Erin Stansbury <ekstansbury@gmail.com>
2021-03-05 05:03:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
+  enable :sessions
+  use Rack::Flash
   configure do
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
-    use Rack::Flash
-    set :session_secret, "secret"
   end
 
   get '/' do
