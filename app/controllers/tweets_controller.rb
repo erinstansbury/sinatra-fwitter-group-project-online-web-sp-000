65e80c36a7dfca73b140c8cadf3d721c6b6facfa
Erin Stansbury <ekstansbury@gmail.com>
2021-03-05 05:23:20 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,142 +1,73 @@
 class TweetsController < ApplicationController
-  enable :sessions
-  use Rack::Flash
-  configure do
-    set :session_secret, "secret"
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get '/' do
-    erb :index
-  end
-
-  get '/signup' do
-    if Helpers.is_logged_in?(session)
-      redirect to '/tweets'
-    end
-
-    erb :"/users/create_user"
-  end
-
-  post '/signup' do
-    params.each do |label, input|
-      if input.empty?
-        flash[:new_user_error] = "Please enter a value for #{label}"
-        redirect to '/signup'
-      end
-    end
-
-    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
-    session[:user_id] = user.id
-
-    redirect to '/tweets'
-  end
-
-  get '/login' do
-    if Helpers.is_logged_in?(session)
-      redirect to '/tweets'
-    end
-
-    erb :"/users/login"
-  end
-
-  post '/login' do
-    user = User.find_by(:username => params["username"])
-
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to '/tweets'
+ get '/tweets' do
+    if session[:user_id]
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
     else
-      flash[:login_error] = "Incorrect login. Please try again."
-      redirect to '/login'
+      redirect '/login'
     end
-  end
-
-  get '/tweets' do
-    if !Helpers.is_logged_in?(session)
-      redirect to '/login'
-    end
-    @tweets = Tweet.all
-    @user = Helpers.current_user(session)
-    erb :"/tweets/tweets"
   end
 
   get '/tweets/new' do
-    if !Helpers.is_logged_in?(session)
-      redirect to '/login'
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      erb :'tweets/new'
+    else
+      redirect '/login'
     end
-    erb :"/tweets/create_tweet"
   end
 
   post '/tweets' do
-    user = Helpers.current_user(session)
-    if params["content"].empty?
-      flash[:empty_tweet] = "Please enter content for your tweet"
-      redirect to '/tweets/new'
+    if params[:content] == ""
+      redirect '/tweets/new'
+    else
+      @tweet = Tweet.new(:content => params[:content])
+      @user = User.find(session[:user_id])
+      @user.tweets.push(@tweet)
+      @user.save
+      redirect '/tweets'
     end
-    tweet = Tweet.create(:content => params["content"], :user_id => user.id)
-
-    redirect to '/tweets'
   end
 
   get '/tweets/:id' do
-    if !Helpers.is_logged_in?(session)
-      redirect to '/login'
+    if session[:user_id]
+      @tweet = Tweet.find(params[:id])
+      @user = User.find(session[:user_id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect '/login'
     end
-    @tweet = Tweet.find(params[:id])
-    erb :"tweets/show_tweet"
   end
 
   get '/tweets/:id/edit' do
-    if !Helpers.is_logged_in?(session)
-      redirect to '/login'
-    end
-    @tweet = Tweet.find(params[:id])
-    if Helpers.current_user(session).id != @tweet.user_id
-      flash[:wrong_user_edit] = "Sorry you can only edit your own tweets"
-      redirect to '/tweets'
-    end
-    erb :"tweets/edit_tweet"
-  end
-
-  patch '/tweets/:id' do
-    tweet = Tweet.find(params[:id])
-    if params["content"].empty?
-      flash[:empty_tweet] = "Please enter content for your tweet"
-      redirect to "/tweets/#{params[:id]}/edit"
-    end
-    tweet.update(:content => params["content"])
-    tweet.save
-
-    redirect to "/tweets/#{tweet.id}"
-  end
-
-  post '/tweets/:id/delete' do
-    if !Helpers.is_logged_in?(session)
-      redirect to '/login'
-    end
-    @tweet = Tweet.find(params[:id])
-    if Helpers.current_user(session).id != @tweet.user_id
-      flash[:wrong_user] = "Sorry you can only delete your own tweets"
-      redirect to '/tweets'
-    end
-    @tweet.delete
-    redirect to '/tweets'
-  end
-
-  get '/users/:slug' do
-    slug = params[:slug]
-    @user = User.find_by_slug(slug)
-    erb :"users/show"
-  end
-
-  get '/logout' do
-    if Helpers.is_logged_in?(session)
-      session.clear
-      redirect to '/login'
+    if session[:user_id]
+      @tweet = Tweet.find(params[:id])
+      @user = User.find(session[:user_id])
+      erb :'/tweets/edit_tweet'
     else
-      redirect to '/'
+      redirect '/login'
+    end
+  end
+
+  #update
+  patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    if params[:content] == ""
+      redirect "/tweets/#{params[:id]}/edit"
+    else
+      @tweet.update(:content => params[:content])
+      redirect '/tweets'
+    end
+  end
+
+  #destroy
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find(params[:id])
+    if session[:user_id] == @tweet.user_id
+      Tweet.destroy(params[:id])
+      redirect '/tweets'
+    else
+      redirect '/tweets'
     end
   end
 
