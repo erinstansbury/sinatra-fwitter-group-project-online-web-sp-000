c547765432e9fbb87a9d97efa92f73e687459f73
Erin Stansbury <ekstansbury@gmail.com>
2021-03-05 04:37:42 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -47,7 +47,7 @@ class TweetsController < ApplicationController
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-    erb :'/users/user'
+    erb :'/users/show'
   end
 
 end
